afd91d57f27d0c2d729a44b870b3569dfe45cece
larafra99 <lara.sofie.franke@hs-furtwangen.de>
2022-06-10 22:58:45 +0200

    going back to version before

--- Code/prototype/prototypev1/pages/index.js
+++ Code/prototype/prototypev1/pages/index.js
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 // TODO startseite überarbeiten
+//TODO Burgermenu 
+
 
 // Get all posts
 export async function getStaticProps() {
@@ -13,9 +15,7 @@ export async function getStaticProps() {
 
   // Loop durch all posts um title und image/video-objekte zubekommen
   const posts = files.map((fileName) => {
-    //const slug = fileName.replace('.md', '','.mdx'); // slug (URL) ohne .md
-    //const slug = fileName.split(".")[0] //lug (URL) ohne .md
-    const slug = fileName
+    const slug = fileName.replace('.md', '','.mdx'); // slug (URL) ohne .md
     const readFile = fs.readFileSync(`posts/index/${fileName}`, 'utf-8'); // Lesen der Datei
     const { data: frontmatter } = matter(readFile); // variable data zu frontmatter umwandeln
     return {
@@ -37,10 +37,10 @@ export default function Home({ posts }) {
     <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0'>
       {posts.map(({ slug, frontmatter }) => (
         <div
-          key={slug.split(".")[0]}
+          key={slug}
           className='border border-gray-200 m-2 rounded-xl shadow-lg overflow-hidden flex flex-col'
         >
-          <Link href={`post/${slug.split(".")[0]}`}>
+          <Link href={`/post/index/${slug}`}>
             <a>
               <Image
                 width={650}
--- Code/prototype/prototypev1/pages/post/[slug].js
+++ Code/prototype/prototypev1/pages/post/[slug].js
@@ -9,10 +9,7 @@ export async function getStaticPaths() {
   const files = fs.readdirSync('posts/index');
   const paths = files.map((fileName) => ({
     params: {
-      //slug: fileName.replace('.mdx', '','.md'),
-      //slug: fileName.split(".")[0],
-      slug: fileName,
-      fileformat: fileName.split(".")[-1],
+      slug: fileName.replace('.md', '','.mdx'),
     },
   }));
   return {
@@ -20,46 +17,9 @@ export async function getStaticPaths() {
     fallback: false,
   };
 }
-/*
-export async function getStaticProps({ params: { slug, fileformat } }) {
-
-  console.log("Hier" + fileformat);
-  if (fileformat == "md"){
-    const fileName = fs.readFileSync(`posts/index/${slug}.md`, 'utf-8');
-    const { data: frontmatter, content } = matter(fileName);
-    return {
-      props: {
-        frontmatter,
-        content,
-      },
-    };
-  } else if(fileformat == "mdx") {
-    const fileName = fs.readFileSync(`posts/index/${slug}.mdx`, 'utf-8'); 
-    const { data: frontmatter, content } = matter(fileName);
-    return {
-      props: {
-        frontmatter,
-        content,
-      },
-    };
-  } else {
-    const fileName = fs.readFileSync(`posts/index/${slug}.md`, 'utf-8');
-    const { data: frontmatter, content } = matter(fileName);
-    return {
-      props: {
-        frontmatter,
-        content,
-      },
-    };
-  }
-  
-  
-}*/
 
 export async function getStaticProps({ params: { slug } }) {
-
- 
-  const fileName = fs.readFileSync(`posts/index/${slug}`, 'utf-8');
+  const fileName = fs.readFileSync(`posts/index/${slug}.md`, 'utf-8');
   const { data: frontmatter, content } = matter(fileName);
   return {
     props: {
@@ -67,10 +27,6 @@ export async function getStaticProps({ params: { slug } }) {
       content,
     },
   };
-  
-  
-  
-  
 }
 
 export default function PostPage({ frontmatter, content }) {
