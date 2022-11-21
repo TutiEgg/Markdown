c00f84fa70af3b3d2f18b3e02f310859646daf26
frankela <lara.sofie.franke@hs-furtwangen.de>
2022-11-21 12:49:52 +0100

    leere ULTags entfernt

--- Code/prototype/prototypev4/components/layout.js
+++ Code/prototype/prototypev4/components/layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children }) {
   
   useEffect(() => {
      
-    var file_name = require('../pages/post/data.json');
+    var file_all = require('../pages/post/data.json');
     // var all_files = file_name.path; 
     // var all_files = require('./navigation_layout.json'); 
     
@@ -26,30 +26,22 @@ export default function Layout({ children }) {
     // https://www.itgeared.com/css-multi-level-navigation-menu-tutorial/
     var element_dict = {};
 
-    for (var i=0; i<file_name.length; i++) {
-      var files_path = file_name[i].path; 
+    for (var i=0; i<file_all.length; i++) {
+      var files_path = file_all[i].path; 
       var path_split_length = files_path.split("/").length;
-  
-      console.log("path",file_name.length);
-      console.log("path",path_split_length);
       
       let parent_div = uld;
-      var ul = document.createElement('ul');
-      parent_div.appendChild(ul);
-      var element = "li";
-      var li_j = document.createElement(element); 
+      var li_j = document.createElement("li"); 
       parent_div.appendChild(li_j); 
-      var ul_j=document.createElement("a");
-      var ul_j_name=file_name[i].name;
-      console.log("NAme",'/post/'+ul_j_name);
-      ul_j.href= '/post/'+ul_j_name;
-      ul_j.setAttribute("id", ul_j_name);
-      ul_j.innerHTML = ul_j_name;
-
-      li_j.appendChild(ul_j)
+      var a_tag =document.createElement("a");
+      a_tag.href= '/post/'+file_all[i].name;
+      //ul_j.href= file_name[i].path;
+      
+      a_tag.setAttribute("id", file_all[i].name);
+      a_tag.innerHTML = file_all[i].name;
+      li_j.appendChild(a_tag)
       parent_div.appendChild(li_j);   
-
-      parent_div = ul_j;
+      parent_div = a_tag;
     }
     nav.appendChild(uld);
   
