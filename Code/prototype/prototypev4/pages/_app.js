e4c40d14e81536290d7006e70ce21c3ed128db90
manzijes <64071693+manzijes@users.noreply.github.com>
2022-12-12 15:59:03 +0100

    farben in extra datei auslagern und einbinden

--- Code/prototype/prototypev4/pages/_app.js
+++ Code/prototype/prototypev4/pages/_app.js
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
+import '../styles/colors.css';
 // import '../styles/globals.css';
 import '../styles/globals_sidebar.css';
 import Layout from '../components/layout';
