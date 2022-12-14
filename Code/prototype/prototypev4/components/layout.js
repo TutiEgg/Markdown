0b71a0fe0b3787eb4656695eefe167b85146f194
frankela <lara.sofie.franke@hs-furtwangen.de>
2022-12-14 22:20:34 +0100

    try to get unpublished html element

--- Code/prototype/prototypev4/components/layout.js
+++ Code/prototype/prototypev4/components/layout.js
@@ -10,10 +10,11 @@ export default function Layout({ children }) {
 
     var nav = document.getElementById("nav");
     var uld = document.createElement("ul");
+    let parent_div = uld;
     uld.setAttribute("id", "main");
     uld.classList.add("navbar-nav", "mr-auto");
     
-
+    var id_array=new Array;
     for (var i=0; i<file_structure.length; i++) {
       console.log(file_structure[i]);
       var key = (Object.keys(file_structure[i]));
@@ -22,43 +23,50 @@ export default function Layout({ children }) {
 
       var navi_name = file_structure[i][key]['navname'];
       var a_id = file_structure[i][key]['filename'];
+      
 
       if(!a_id){
         console.log("1");
-        let parent_div = uld;
         var ul_cat = document.createElement("ul"); 
-        parent_div.appendChild(ul_cat);
-        ul_cat.innerHTML= navi_name; 
+        ul_cat.innerHTML= navi_name;
+        ul_cat.setAttribute("id", navi_name);
+        console.log("2",id_array[2])
+        
+        
+        if(key_split.length==1){
+          parent_div.appendChild(ul_cat);
+          id_array=[];
+        }
+        else{
+          var parent = document.getElementById(id_array[1]);
+          //parent.appendChild(ul_cat);
+          console.log("parent",parent);
+        }
+        id_array.push(navi_name);
+        console.log("Array",id_array);
+        
       }
       // der teil rekursiv 
+      // while key.length stays the same append li else rekursion 
+      // ignorieren der Zahlenreihenfolge, wichtig ?
       // leerer Namensordner ahref lösung 
       else{
         console.log("2");
         var a_href=  a_id.toString().split(".")[0];
-        var li_j = document.createElement("li"); 
-        ul_cat.appendChild(li_j); 
+        var li_j = document.createElement("li");        
         var a_tag =document.createElement("a");
         a_tag.href= '/post/'+a_href;
         a_tag.setAttribute("id", a_id);
         a_tag.innerHTML = navi_name;
         li_j.appendChild(a_tag);
-      }
 
-
-      // let parent_div = uld;
-      //   var li_j = document.createElement("li"); 
-      //   parent_div.appendChild(li_j); 
-      //   var a_tag =document.createElement("a");
-      //   // a_tag.href= '/post/'+file_all[i].name;
-      //   // //a_tag.href= '/'+file_all[i].path;
-        
-      //   a_tag.setAttribute("id", file_structure[i].navname);
-      //   a_tag.innerHTML = file_all[i].name;
-      //   li_j.appendChild(a_tag);
-      //   parent_div.appendChild(li_j);   
-      //   parent_div = a_tag;
-      
-      
+        if(key_split.length==1){
+          parent_div.appendChild(li_j);
+        } 
+        else{
+          ul_cat.appendChild(li_j); 
+        }
+      }  
     }
     nav.appendChild(uld);     
   }, [])
